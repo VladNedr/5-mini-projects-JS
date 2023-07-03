eae0e23a3c5155075b9254cbfc6f33b89c6ed74c
VladNedr <nedr2517@mail.ru>
2023-07-03 13:36:05 +0300

    add : ArrowUp and ArrowDown key

--- Slider images 03/app.js
+++ Slider images 03/app.js
@@ -12,11 +12,20 @@ let activeSlideIndex = 0;
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
 upBtn.addEventListener("click", () => {
-  changeSlide("up")
+  changeSlide("up");
 });
 
 downBtn.addEventListener("click", () => {
-  changeSlide("down")
+  changeSlide("down");
+});
+
+// функционал , чтобы при нажатии стрелок на клавиатуре ,слайдер тоже работал.
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    changeSlide("up");
+  } else if (event.key === "ArrowDown") {
+    changeSlide("down");
+  }
 });
 
 function changeSlide(direction) {
@@ -37,4 +46,4 @@ function changeSlide(direction) {
   mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
 
   sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
-};
+}
